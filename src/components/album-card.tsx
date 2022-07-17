73ddcff3319030a7ae9ecf465e0e3a7b71e6ff09
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-07-16 21:29:11 -0600

    🐛 Fix hearts

--- src/components/album-card.tsx
+++ src/components/album-card.tsx
@@ -67,24 +67,26 @@ const AlbumCard: React.FC<{ id: string; user: string }> = ({ id, user }) => {
                 className='flex justify-between w-full py-1 px-2 rounded-md hover:bg-base-200'>
                 <p>{track.name}</p>
                 <div className='flex'>
-                  {track.fav.slice(1).map((fav: any) => (
-                    <div className='tooltip tooltip-primary' data-tip={fav.name} key={fav.id}>
-                      <svg
-                        key={fav.id}
-                        xmlns='http://www.w3.org/2000/svg'
-                        className='h-6 w-6 fill-primary'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        stroke='currentColor'
-                        strokeWidth={2}>
-                        <path
-                          strokeLinecap='round'
-                          strokeLinejoin='round'
-                          d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
-                        />
-                      </svg>
-                    </div>
-                  ))}
+                  {track.fav
+                    .filter((fav: any) => fav.id !== session?.id)
+                    .map((fav: any) => (
+                      <div className='tooltip tooltip-primary' data-tip={fav.name} key={fav.id}>
+                        <svg
+                          key={fav.id}
+                          xmlns='http://www.w3.org/2000/svg'
+                          className='h-6 w-6 fill-primary'
+                          fill='none'
+                          viewBox='0 0 24 24'
+                          stroke='currentColor'
+                          strokeWidth={2}>
+                          <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
+                          />
+                        </svg>
+                      </div>
+                    ))}
                   <button
                     onClick={() => {
                       track.fav.map((fav: any) => fav.id).some((v: any) => session?.id === v)
